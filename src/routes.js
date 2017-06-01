f3e1a35071e9d208b938719134f6cce690808daf
Moe <mohammed.elsharif@gmail.com>
2017-06-01 12:57:36 +0200

    -New route for Layout

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,13 @@
 import App from './components/App';
 import NotFound from './components/NotFound';
+import htmllayout from './components/htmllayout';
+
 
 // Routes for redux-router-kit
 const routes = {
   '/': App,
   '/not-found': NotFound,
+  '/layout': htmllayout,
 };
 
 export default routes;
