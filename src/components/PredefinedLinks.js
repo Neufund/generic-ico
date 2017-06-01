34c8557c0efbfb9fcc49b896ba3bee9e1174c205
Tomek Banach <tomek@banciur.org>
2017-06-01 18:24:25 +0200

    Add predefined links

---
+++ src/components/PredefinedLinks.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Link from '../components/TempLink';
+
+// TODO: Is it possible to rewrite this without usage of return but keeping propTypes check?
+const LinkCreator = (path) => {
+  const func = ({ children, ...props }) => <Link {...props} path={path}>{children}</Link>;
+  func.propTypes = {
+    children: PropTypes.node.isRequired,
+  };
+  return func;
+};
+
+export const LoginLink = LinkCreator('/login');
+export const LoginEmailLink = LinkCreator('/login/email');
+export const LoginNanoLink = LinkCreator('/login/nano');
+export const RegisterEmailLink = LinkCreator('/register/email');
+export const KYCLink = LinkCreator('/kyc');
