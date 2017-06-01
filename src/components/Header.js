1396e79eed739058dd7fc396a4d5bb8f4834c304
Tomek Banach <tomek@banciur.org>
2017-06-01 15:37:49 +0200

    Add temporary Link component till its ready in dapp-starter-kit

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,41 +1,14 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
-import { routeTo } from 'redux-router-kit';
-
-const mapDispatchToProps = dispatch => ({
-  goToLink: (id) => {
-    dispatch(routeTo(id));
-  },
-});
-
-const LinkView = ({ goToLink, caption, url }) =>
-  (<a
-    tabIndex="0"
-    role="link"
-    onClick={(event) => {
-      event.preventDefault();
-      goToLink(url);
-    }}
-  >{caption}</a>);
-
-LinkView.propTypes = {
-  goToLink: PropTypes.func.isRequired,
-  url: PropTypes.string.isRequired,
-  caption: PropTypes.string.isRequired,
-};
-
-const Link = connect(
-  null,
-  mapDispatchToProps
-)(LinkView);
+import Link from '../components/TempLink';
 
 const Header = () => (
   <header>
     <div>Header</div>
-    <Link url="/login" caption="Login" /> <br />
-    <Link url="/startupintro" caption="Startups" /> <br />
-    <Link url="/investorintro/1" caption="Investors" />
+    <nav>
+      <ul>
+        <li><Link path="/login">Login</Link> <br /></li>
+      </ul>
+    </nav>
     <hr />
   </header>);
 
