e4fd48732282fd829e15b3b3b0bfad0240aa02fa
Moe <mohammed.elsharif@gmail.com>
2017-06-07 19:13:53 +0200

    -Review fixes

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,20 +2,20 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './Button.scss';
 
-const Button = ({ callback, children, ...props }) =>
+const Button = ({ onClick, children, ...props }) =>
 (
   <button
     className={styles.Button}
     tabIndex="-1"
     {...props}
-    onClick={() => callback()}
+    onClick={() => onClick()}
   >
     {children}
   </button>
   );
 
 Button.propTypes = {
-  callback: PropTypes.func.isRequired,
+  onClick: PropTypes.func.isRequired,
   children: PropTypes.node.isRequired,
 };
 
