959395f12dd5ccd3913743ada499f75b476e1740
Tomek Banach <tomek@banciur.org>
2017-06-07 11:55:19 +0200

    Add Footer to Layout

--- src/views/Layout.js
+++ src/views/Layout.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Header from '../components/Header';
+import Footer from '../components/Footer';
 
 const Layout = ({ children }) => (<div>
   <Header />
   <div>
     {children}
   </div>
+  <Footer />
 </div>);
 
 Layout.propTypes = {
