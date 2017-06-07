b1fa953571a8daa1261d87f78f8e89c7923771dc
Remco Bloemen <remco-git@xn--2-umb.com>
2017-06-07 12:05:51 +0100

    Refactor routes

--- src/routes.js
+++ src/routes.js
@@ -10,64 +10,20 @@ import KYC from './views/KYC';
 import Test from './views/Test';
 import NotFound from './views/NotFound';
 
-const MainPage = () => (
-  <Layout>
-    <QuotedStartups />
-  </Layout>
-);
+const page = View => () => <Layout><View /></Layout>;
 
-const TestPage = () => (
-  <Layout>
-    <Test />
-  </Layout>
-);
-
-const LoginPage = () => (
-  <LayoutEntry>
-    <Login />
-  </LayoutEntry>
-);
-
-const LoginNanoPage = () => (
-  <Layout>
-    <LoginNano />
-  </Layout>
-);
-
-const RegisterPage = () => (
-  <Layout>
-    <Register />
-  </Layout>
-);
-
-const ConfirmationEmailPage = () => (
-  <Layout>
-    <ConfirmEmail />
-  </Layout>
-);
-
-const KYCPage = () => (
-  <Layout>
-    <KYC />
-  </Layout>
-);
-
-const NotFoundPage = () => (
-  <Layout>
-    <NotFound />
-  </Layout>
-);
+const entryPage = View => () => <LayoutEntry><View /></LayoutEntry>;
 
 // Routes for redux-router-kit
 const routes = {
-  '/': MainPage,
-  '/test': TestPage,
-  '/login': LoginPage,
-  '/login/nano': LoginNanoPage,
-  '/register': RegisterPage,
-  '/register/email/:confirmationId': ConfirmationEmailPage,
-  '/kyc': KYCPage,
-  '/not-found': NotFoundPage,
+  '/': page(QuotedStartups),
+  '/test': page(Test),
+  '/login': entryPage(Login),
+  '/login/nano': page(LoginNano),
+  '/register': page(Register),
+  '/register/email/:confirmationId': page(ConfirmEmail),
+  '/kyc': page(KYC),
+  '/not-found': page(NotFound),
 };
 
 export default routes;
