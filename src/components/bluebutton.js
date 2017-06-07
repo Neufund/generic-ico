9c1f1f1bc21da3642c0da71d6e88af8fa33827b8
Moe <mohammed.elsharif@gmail.com>
2017-06-07 16:13:30 +0200

    -Add label to button -Add default value to button -Add proptypes -Add Additional inline props

--- src/components/bluebutton.js
+++ src/components/bluebutton.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+
 import styles from './bluebutton.scss';
 
-const Button = () => (
-  <div className={styles.our_button}>
-test
-</div>);
+const Button = ({ label, ...props }) => (
+  <div className={styles.our_button} {...props}>
+    {label}
+  </div>);
+
+Button.propTypes = {
+  label: PropTypes.string.isRequired,
+};
+
+Button.defaultProps = {
+  label: 'Name me!',
+};
 
 module.exports = Button;
 
