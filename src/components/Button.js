deec940abdb578258790facbd560e01ed838c05d
Moe <mohammed.elsharif@gmail.com>
2017-06-07 19:30:18 +0200

    -fixes

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,11 +2,11 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './Button.scss';
 
-const Button = ({ onClick, children, ...props }) =>
+const Button = ({ onClick, children, tabIndex, ...props }) =>
 (
   <button
     className={styles.Button}
-    tabIndex="-1"
+    tabIndex={tabIndex}
     {...props}
     onClick={() => onClick()}
   >
@@ -17,8 +17,12 @@ const Button = ({ onClick, children, ...props }) =>
 Button.propTypes = {
   onClick: PropTypes.func.isRequired,
   children: PropTypes.node.isRequired,
+  tabIndex: PropTypes.string.isRequired,
 };
 
+Button.defaultProps = {
+  tabIndex: '0',
+};
 module.exports = Button;
 
 // TODO: Add additional abstractions colors modes
