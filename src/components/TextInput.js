e41ba87367250485136b459ee55b1d2fe5f51d02
Moe <mohammed.elsharif@gmail.com>
2017-06-13 15:05:49 +0200

    added new component

--- src/components/TextInput.js
+++ src/components/TextInput.js
@@ -1,28 +1,19 @@
 import React from 'react';
-import { Field, reduxForm, propTypes } from 'redux-form';
+import { Field, propTypes } from 'redux-form';
 import { TextField } from 'redux-form-material-ui';
 
 
-const SimpleForm = (props) => {
-  const { handleSubmit } = props;
-  return (
-    <form onSubmit={handleSubmit}>
-      <div>
-        <div>
-          <Field
-            name="firstName"
-            component={TextField}
-            type="text"
-            placeholder="First Name"
-          />
-        </div>
-      </div>
-    </form>
+const TextInput = ({ name, type, placeholder, ...props }) => (
+  <Field
+    name={name}
+    component={TextField}
+    type={type}
+    {...props}
+    placeholder={placeholder}
+  />
   );
-};
-SimpleForm.propTypes = {
+TextInput.propTypes = {
   ...propTypes,
 };
-export default reduxForm({
-  form: 'simple', // a unique identifier for this form
-})(SimpleForm);
+
+export default TextInput;
