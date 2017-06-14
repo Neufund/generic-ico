3de69a47c10d737eca79c36fda1585f16849e26c
Moe <mohammed.elsharif@gmail.com>
2017-06-14 17:51:49 +0200

    Initial Commit

---
+++ src/components/QrComponent.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { routeTo } from 'redux-router-kit';
+
+const LinkView = ({ targetTo, children, path, tabIndex, ...props }) =>
+  (<a
+    tabIndex={tabIndex}
+    role="link"
+    {...props}
+    onClick={(event) => {
+      event.preventDefault();
+      targetTo(path);
+    }}
+    style={{ cursor: 'pointer' }}
+  >{children}</a>);
+
+LinkView.propTypes = {
+  targetTo: PropTypes.func.isRequired,
+  path: PropTypes.string.isRequired,
+  children: PropTypes.node.isRequired,
+  tabIndex: PropTypes.string.isRequired,
+};
+
+LinkView.defaultProps = {
+  tabIndex: '0',
+};
+
+export default connect(
+  null,
+  dispatch => ({
+    targetTo: path => dispatch(routeTo(path)),
+  })
+)(LinkView);
