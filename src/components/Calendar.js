7fc006d1375d8d9ad2f355b4a0e4e2660db671f6
Moe <mohammed.elsharif@gmail.com>
2017-06-19 15:14:29 +0200

    Added Component

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -1,3 +1,34 @@
-import QRCode from 'qrcode.react';
+import React from 'react';
+import { Field } from 'redux-form';
+import DatePicker from 'material-ui/DatePicker';
+import PropTypes from 'prop-types';
 
-export default QRCode;
+const mapError = ({
+    meta: { touched, error, warning } = {},
+    input,
+    ...props
+  }) =>
+  (touched && (error || warning)
+    ? {
+      ...props,
+      ...input,
+      errorText: error || warning,
+    }
+    : { ...input, ...props });
+
+const RenderTextField = props =>
+  <DatePicker {...mapError(props)} />;
+
+const TextInput = ({ name, ...props }) => (
+  <Field
+    {...props}
+    name={name}
+    format={null}
+    component={RenderTextField}
+  />
+  );
+TextInput.propTypes = {
+  name: PropTypes.string.isRequired,
+};
+
+export default TextInput;
