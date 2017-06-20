048549e7f964c961313db88a413361dc94087d5b
Moe <mohammed.elsharif@gmail.com>
2017-06-20 11:02:55 +0200

    Basic Upload

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -1,7 +1,9 @@
 import React from 'react';
+import Dropzone from 'react-dropzone';
 import PropTypes from 'prop-types';
-import styles from './Buttons.scss';
+import styles from './Link.scss';
 
+<<<<<<< HEAD
 const buttonCreator = (style) => {
   const Button = ({ onClick, children, tabIndex, type }) => (
     <button
@@ -33,3 +35,22 @@ export const RoundButton = buttonCreator(styles.roundButton);
 export const RoundHeaderButton = buttonCreator(styles.roundHeaderButton);
 export const BorderButton = buttonCreator(styles.borderButton);
 export const BorderHeaderButton = buttonCreator(styles.borderHeaderButton);
+=======
+const Upload = ({ onDrop, children, ...props }) =>
+  (<Dropzone
+    className={styles.link}
+    // What do we accept as valid?
+    accept="image/jpeg, image/png"
+    {...props}
+    onDrop={onDrop}
+  >
+    <div>{children}</div>
+  </Dropzone>);
+
+Upload.propTypes = {
+  onDrop: PropTypes.func.isRequired,
+  children: PropTypes.node.isRequired,
+};
+
+export default Upload;
+>>>>>>> Basic Upload
