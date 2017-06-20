aed26fe71060cb9c3bd977d3832f2ba32e190e52
Tomek Banach <tomek@banciur.org>
2017-06-20 12:50:34 +0200

    Add CircularProgress to Timer component

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import CircularProgress from 'material-ui/CircularProgress';
 
 
 import styles from './Timer.scss';
 
 // TODO: this is just placeholder it needs to be implemented correctly
 export default () =>
-  (<p className={styles.timer}>
-    You have <span className={styles.countdown}>8 seconds</span> left
-  </p>);
+  (<div className={styles.timer}>
+    <CircularProgress size={25} />
+    <p className={styles.text}>
+      You have <span className={styles.countdown}>8 seconds</span> left
+    </p>
+  </div>);
