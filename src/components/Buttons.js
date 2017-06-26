c4c2c143fa6c47ba6a7ff623453dfc27e04002b1
Moe <mohammed.elsharif@gmail.com>
2017-06-26 14:51:42 +0200

    -Added Translations -Component changes
    z

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -1,9 +1,7 @@
 import React from 'react';
-import Dropzone from 'react-dropzone';
 import PropTypes from 'prop-types';
-import styles from './Link.scss';
+import styles from './Buttons.scss';
 
-<<<<<<< HEAD
 const buttonCreator = (style) => {
   const Button = ({ onClick, children, tabIndex, type }) => (
     <button
@@ -35,22 +33,3 @@ export const RoundButton = buttonCreator(styles.roundButton);
 export const RoundHeaderButton = buttonCreator(styles.roundHeaderButton);
 export const BorderButton = buttonCreator(styles.borderButton);
 export const BorderHeaderButton = buttonCreator(styles.borderHeaderButton);
-=======
-const Upload = ({ onDrop, children, ...props }) =>
-  (<Dropzone
-    className={styles.link}
-    // What do we accept as valid?
-    accept="image/jpeg, image/png"
-    {...props}
-    onDrop={onDrop}
-  >
-    <div>{children}</div>
-  </Dropzone>);
-
-Upload.propTypes = {
-  onDrop: PropTypes.func.isRequired,
-  children: PropTypes.node.isRequired,
-};
-
-export default Upload;
->>>>>>> Basic Upload
--- src/components/FieldImage.js
+++ src/components/FieldImage.js
@@ -2,48 +2,44 @@ import React from 'react';
 import Dropzone from 'react-dropzone';
 import { Field } from 'redux-form';
 import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { getTranslator } from '../reducers/translation';
 import styles from './FieldImage.scss';
 
-const oFReader = new FileReader(); // eslint-disable-line no-undef
+const fileInstance = new FileReader(); // eslint-disable-line no-undef
+const maxFilesize = 3000000;
 
-const DropzoneImage = ({ input }) => (
+const DropzoneImage = ({ i18n, input }) => (
   <div>
     <Dropzone
-      accept={'image/*'}
-      maxSize={300000}
+      accept={'image/*,application/pdf'}
+      maxSize={maxFilesize}
       multiple={false}
       className={styles.overide}
       name={input.name}
-      onDrop={(fileToUpload) => {
-        oFReader.readAsDataURL(fileToUpload[0]);
-        oFReader.onloadend = () => input.onChange(oFReader.result);
+      onDropAccepted={(imageData) => {
+        fileInstance.readAsDataURL(imageData[0]);
+        fileInstance.onloadend = () => input.onChange(fileInstance.result);
       }}
     >
       <div>
-        <p className={styles.header} >
-          <i className={`material-icons ${styles.logo}`}>publish</i> &nbsp; &nbsp; Click here to upload a file </p>
-        <p className={styles.body} >
-         Your file should not be bigger than 3 MB it can be .png .jpg
-        </p>
+        <div className={styles.header}>
+          <div className={`material-icons ${styles.logo}`}>{i18n('publish')}</div>{i18n('Click here to upload a file')}
+        </div>
+        <p className={styles.body}>{i18n('Your file should not be bigger than 3 MB it can be .png .jpg')}</p>
       </div>
     </Dropzone>
-    {input.value && (
-      <div>
-        { <img
-          style={{ maxHeight: '200px' }}
-          alt={input.name}
-          src={input.value}
-        />} </div>)}
+    {input.value.includes('image') && <img alt={input.name} src={input.value} style={{ maxHeight: '200px' }} />}
+    {input.value.includes('pdf') && <div className="material-icons" style={{ fontSize: '70px' }} > picture_as_pdf </div>}
   </div>);
+
 DropzoneImage.propTypes = {
+  i18n: PropTypes.func.isRequired,
   input: PropTypes.shape({
     name: PropTypes.string.isRequired,
-    value: PropTypes.oneOfType([
-      PropTypes.string.isRequired,
-      PropTypes.arrayOf(PropTypes.object).isRequired]),
+    value: PropTypes.string.isRequired,
   }).isRequired,
 };
-
 
 const FieldImage = ({ name, ...props }) => (
   <Field
@@ -52,10 +48,10 @@ const FieldImage = ({ name, ...props }) => (
     component={DropzoneImage}
   />
 );
+
 FieldImage.propTypes = {
   name: PropTypes.string.isRequired,
 };
-export default FieldImage;
 
-// TODO: clean code
-// TODO: Add translation
+export default connect(state => ({
+  i18n: getTranslator(state) }))(FieldImage);
