509d00242d0a9c1225ad9e06c763523cb107d707
Tomek Banach <tomek@banciur.org>
2017-06-27 13:56:39 +0200

    Move some inline styling to stylesheet in HeaderRegisterWelcome component

--- src/components/HeaderRegisterWelcome.js
+++ src/components/HeaderRegisterWelcome.js
@@ -2,21 +2,21 @@ import React from 'react';
 import { Grid, Row, Col } from 'react-flexbox-grid';
 import IconButton from 'material-ui/IconButton';
 
-import styles from './HeaderRegisterWelcome.scss';
-import common from '../styles/common.scss';
+import { header, content, user, iconStyle } from './HeaderRegisterWelcome.scss';
+import { layoutWidthLimiter } from '../styles/common.scss';
 
 // TODO: This component need to be finished for now its just placeholder to show on one of pages
 export default () =>
   (<header>
-    <div className={common.layoutWidthLimiter}>
-      <div className={styles.header}>
+    <div className={layoutWidthLimiter}>
+      <div className={header}>
         <Grid fluid>
           <Row>
-            <Col lgOffset={1} lg={11} className={styles.content}>
-              <IconButton iconClassName="material-icons" iconStyle={{ color: '#fff' }}>
+            <Col lgOffset={1} lg={11} className={content}>
+              <IconButton iconClassName={`material-icons ${iconStyle}`} >
                 person
               </IconButton>
-              <div className={styles.user}>Marcin Rudolf</div>
+              <div className={user}>Marcin Rudolf</div>
             </Col>
           </Row>
         </Grid>
