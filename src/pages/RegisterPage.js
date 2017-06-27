30134882128e0dd1f1de09e08d6814ad135a420e
Moe <mohammed.elsharif@gmail.com>
2017-06-27 17:55:12 +0200

    -Added redux Form to Register Page -Added Style for register page -Initial Style

--- src/pages/RegisterPage.js
+++ src/pages/RegisterPage.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import { Grid, Row, Col } from 'react-flexbox-grid';
+import { reduxForm } from 'redux-form';
+import PropTypes from 'prop-types';
 import HeaderRegistration from '../components/HeaderRegistration';
+import { SquareButton } from '../components/Buttons';
+import TextField from '../components/FieldText';
 import Footer from '../components/Footer';
-
 import common from '../styles/common.scss';
+import RegisterPage from './RegisterPage.scss';
 
-export default () =>
+export const RegisterFormView = ({ handleSubmit }) =>
   (<div className={common.layoutAppContainer}>
     <HeaderRegistration />
     <div className={`${common.layoutContentArea} ${common.gradientContentArea}`}>
@@ -15,7 +19,16 @@ export default () =>
           <Row>
             <Col lgOffset={3} lg={6}>
               <div className={common.whiteArea}>
-                content
+                <div className={RegisterPage.columnArea}>
+                  <h2>Create your private account first</h2>
+                  <form onSubmit={handleSubmit}>
+                    <p>
+                      <TextField name={'firstname'} type={'text'} placeholder={'Choose your most reliable email address'} />
+                    </p>
+                    <SquareButton type={'submit'} > Get started </SquareButton>
+                    <p> Bt </p>
+                  </form>
+                </div>
               </div>
             </Col>
           </Row>
@@ -24,3 +37,13 @@ export default () =>
       <Footer />
     </div>
   </div>);
+
+RegisterFormView.propTypes = {
+  handleSubmit: PropTypes.func.isRequired,
+};
+
+export const LoginForm = reduxForm({
+  form: 'RegisterForm', // a unique identifier for this form
+})(RegisterFormView);
+
+export default LoginForm;
