9bac11b838cf901b57c1a50f8b0346bd692964b2
Moe <mohammed.elsharif@gmail.com>
2017-07-03 17:28:30 +0200

    -Added Translations -Connected links -CSS changes

--- src/pages/RegisterPage.js
+++ src/pages/RegisterPage.js
@@ -2,39 +2,42 @@ import React from 'react';
 import { Grid, Row, Col } from 'react-flexbox-grid';
 import { reduxForm } from 'redux-form';
 import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { getTranslator } from '../reducers/translation';
 import HeaderRegistration from '../components/HeaderRegistration';
 import { SquareButton } from '../components/Buttons';
 import TextField from '../components/FieldText';
 import Footer from '../components/Footer';
 import common from '../styles/common.scss';
 import RegisterPageStyle from './RegisterPage.scss';
+import { LoginLink } from '../components/PredefinedLinks';
 
-export const RegisterFormView = ({ handleSubmit }) =>
+export const RegisterFormView = ({ handleSubmit, i18n }) =>
   (<div className={common.layoutAppContainer}>
     <HeaderRegistration />
     <div className={`${common.layoutContentArea} ${common.gradientContentArea}`}>
       <div className={common.layoutWidthLimiter}>
-
         <Grid fluid>
           <Row>
             <Col lgOffset={3} lg={6}>
               <div className={common.whiteArea}>
                 <div className={RegisterPageStyle.columnArea}>
-                  <h2>Create your private account first</h2>
+                  <h2>{i18n('Create your private account first')}</h2>
                   <form onSubmit={handleSubmit}>
                     <TextField
                       name={'email'}
-                      style={{ marginRight: 'auto',
-                        marginLeft: 'auto',
+                      style={{
+                        marginLeft: '2rem',
                         borderDown: '0.2rem',
                         display: 'block',
                         width: '70%' }}
                       type={'text'}
-                      placeholder={'Choose your most reliable email address'}
+                      placeholder={i18n('Choose your most reliable email address')}
                     />
                     <SquareButton type={'submit'} > Get started </SquareButton>
-                    <p> By clicking get started you agree to Neufunds
-                        Platforms Terms of service and Privacy Policy</p>
+                    <p> {i18n('By clicking get started you agree to Neufunds Platforms')}
+                      <LoginLink>{i18n('Terms of service')}</LoginLink> {i18n('and')} <LoginLink>
+                        {i18n('Privacy Policy')}</LoginLink></p>
                   </form>
                 </div>
               </div>
@@ -48,13 +51,20 @@ export const RegisterFormView = ({ handleSubmit }) =>
 
 RegisterFormView.propTypes = {
   handleSubmit: PropTypes.func.isRequired,
+  i18n: PropTypes.func.isRequired,
 };
 
 export const RegisterForm = reduxForm({
   form: 'RegisterForm', // a unique identifier for this form
 })(RegisterFormView);
 
-export const RegisterPageInstance = () =>
-  <RegisterForm onSubmit={console.log} />;
+export const RegisterPageInstance = ({ i18n }) =>
+  <RegisterForm onSubmit={console.log} i18n={i18n} />;
 
-export default RegisterPageInstance;
+RegisterPageInstance.propTypes = {
+  i18n: PropTypes.func.isRequired,
+};
+
+export default connect(
+  state => ({ i18n: getTranslator(state) })
+)(RegisterPageInstance);
